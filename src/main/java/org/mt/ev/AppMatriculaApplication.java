86663e7e93a4112af98d782ef6e4a80c6c56fe04
robert <254313230+robertomosquera-dev@users.noreply.github.com>
2026-05-21 19:50:44 -0500

    inital commit

---
+++ src/main/java/org/mt/ev/AppMatriculaApplication.java
@@ -0,0 +1,13 @@
+package org.mt.ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMatriculaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMatriculaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mt/ev/AppMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mt.ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMatriculaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
